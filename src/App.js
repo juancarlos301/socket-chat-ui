165ead89d68b4ce5168560f0b16f56859836a162
juancarlos301 <juancarlos.31.2002@gmail.com>
2023-09-20 17:30:43 -0500

    add socket begin

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import io from "socket.io-client";
+//import "./App.css";
+import { useEffect, useState } from "react";
+
+const socket = io.connect("http://localhost:3001");
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [room, setRoom] = useState("");
+
+  const [messageReceived, setMessageReceived] = useState("");
+
+  const sendMessages = () => {
+    socket.emit("send_message", { message, room });
+  };
+
+  const joinRoom = () => {
+    socket.emit("join_room", room);
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageReceived(data.message);
+    });
+    // eslint-disable-next-line
+  }, [socket]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        placeholder="put room"
+        onChange={(e) => setRoom(e.target.value)}
+        value={room}
+        onKeyDown={(e) => {
+          console.log(e.key);
+          if (e.key === "Enter") {
+            joinRoom();
+            setRoom("");
+          }
+        }}
+      />
+      <button onClick={joinRoom}>send message</button>
+      <input
+        placeholder="message"
+        onChange={(e) => setMessage(e.target.value)}
+        value={message}
+        onKeyDown={(e) => {
+          console.log(e.key);
+          if (e.key === "Enter") {
+            sendMessages();
+            setMessage("");
+          }
+        }}
+      />
+      <button onClick={sendMessages}>send message</button>
+      <h1>messages</h1>
+      {messageReceived}
     </div>
   );
 }
